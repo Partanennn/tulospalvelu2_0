f7b091a7213358cdeaef6d1890d4134172c0f2cc
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-09-16 14:53:09 +0300

    Design: Tailwind config primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary-100": "#E5F1FF",
+      "primary-200": "#BFDBFF",
+      "primary-300": "#8EB7ED",
+      "primary-400": "#6A97D4",
+      "primary-500": "#4A7ABA",
+      "primary-600": "#2056A1",
+      "primary-700": "#002E6D",
+      "primary-800": "#002454",
+      "primary-900": "#000E21",
+    },
     extend: {
       colors: {
         background: "var(--background)",

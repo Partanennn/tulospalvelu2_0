c691cf3c7aecbd42e214ee1c0fa7163719ace960
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-09-21 23:13:03 +0300

    Added selected TextButton, set selected page to be shown on nav

---
+++ utils/navItems.ts
@@ -0,0 +1,26 @@
+type NavbarItem = {
+  url: string;
+  text: string;
+};
+export const navbarItems: NavbarItem[] = [
+  {
+    text: "Etusivu",
+    url: "/",
+  },
+  {
+    text: "Otteluohjelma",
+    url: "/schedule",
+  },
+  {
+    text: "Säännöt",
+    url: "/rules",
+  },
+  {
+    text: "Pelaajat",
+    url: "/players",
+  },
+  {
+    text: "Tilastot",
+    url: "/stats",
+  },
+];

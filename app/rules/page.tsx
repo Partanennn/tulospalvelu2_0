3e02e71cd6cd81a5b2e140ae8c0cb6a7cce02f72
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-09-24 20:30:50 +0300

    Rules and info is responsive

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -3,7 +3,7 @@ import SerieInfo from "@/components/SerieInfo";
 
 const Saannot = () => {
   return (
-    <div className="flex flex-row justify-evenly pt-7">
+    <div className="flex flex-col items-center gap-5 sm:flex-row justify-evenly pt-7">
       <SerieInfo />
       <Rules />
     </div>

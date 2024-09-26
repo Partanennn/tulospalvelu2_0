9567f89d32c9f9ce6fc464f7f8205716658bb36e
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-09-26 08:15:01 +0300

    WIP2

--- app/team/[id]/page.tsx
+++ app/team/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useFetch from "@/hooks/useFetch";
+import { useSeasonStore } from "@/stores/season-store";
 
 type Params = {
   id: string;
@@ -11,10 +12,12 @@ type Props = {
 };
 
 const TeamIdPage = ({ params: { id } }: Props) => {
+  const { selectedSeason } = useSeasonStore();
   const { data } = useFetch("/api/teaminfo", {
     method: "POST",
     body: JSON.stringify({
       teamid: id,
+      seasonNumber: selectedSeason,
     }),
   });
   return <div>Team page::DDD {id}</div>;

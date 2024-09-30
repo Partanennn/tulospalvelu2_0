13e62ed39aefe32d17ffaac89eaebfd4043ed0ba
Aleksi Partanen <aleksi.partanenn17@gmail.com>
2024-09-30 17:28:41 +0300

    Cleaning, standings change

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import GamesTable from "@/components/GamesTable";
 import PlayerTotalPoints from "@/components/Players/PlayerTotalPoints";
 import Scoreboard from "@/components/Standings";
 
+export const revalidate = 60;
+
 const Tulospalvelu = async () => {
   return (
     <div className="flex flex-col flex-wrap justify-evenly items-center pt-7 gap-4 2xl:flex-row 2xl:items-start">

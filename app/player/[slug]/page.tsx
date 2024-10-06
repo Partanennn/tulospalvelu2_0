c1f390d977944c5260d101e0b292307c7b374c36
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-10-06 15:57:21 +0300

    Removed not needed suspense

--- app/player/[slug]/page.tsx
+++ app/player/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import PlayerInfo from "@/components/Player/PlayerInfo";
-import { Suspense } from "react";
 
 type PlayerSlugPageProps = {
   params: {
@@ -10,9 +9,7 @@ type PlayerSlugPageProps = {
 const PlayerSlugPage = ({ params: { slug } }: PlayerSlugPageProps) => {
   return (
     <div className="flex justify-center">
-      <Suspense>
-        <PlayerInfo playerId={slug ?? ""} />
-      </Suspense>
+      <PlayerInfo playerId={slug ?? ""} />
     </div>
   );
 };

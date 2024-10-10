5402edb0bdc98de1c0c6eac32725aa2d962120b4
Aleksi Partanen <44446649+Partanennn@users.noreply.github.com>
2024-10-10 19:53:31 +0300

    Better front page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ export const revalidate = 60;
 
 const Tulospalvelu = async () => {
   return (
-    <div className="flex flex-col justify-evenly items-center pt-7 gap-4 large-desktop:flex-row large-desktop:items-start">
-      <Scoreboard />
-      <PlayerTotalPoints />
+    <div className="flex flex-col justify-evenly items-center pt-7 gap-4 desktop:flex-row desktop:items-start">
+      <div className="flex flex-col gap-2 items-center">
+        <Scoreboard />
+        <PlayerTotalPoints />
+      </div>
       <GamesTable header="Tulevat Ottelut" gameDays="incoming" />
     </div>
   );
